72dfb93106e540d180d2e74b55f788f13e720571
Trent Wesley <tswesley1@buffs.wtamu.edu>
2026-09-05 17:16:13 -0500

    Display the list on the home page

--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,19 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "home.html")
+    goals = [
+        {"name": "Complete my Information Systems degree", "completed": False},
+        {"name": "Squat 600 pounds", "completed": False},
+        {"name": "Become a firefighter", "completed": False},
+        {"name": "Learn to juggle", "completed": True},
+        {"name": "Play the guitar more", "completed": True},
+    ]
+
+    context = {
+        "goals": goals,
+    }
+
+    return render(request, "home.html", context)
 
 
 def about(request):
